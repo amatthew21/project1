e96e32c866398be132a5b4f20d2f71c18605396f
alvar <alvar@holoPasu>
2023-01-18 00:00:50 -0500

    Initial Commit Message

--- program1/src/program1/testprogram.java
+++ program1/src/program1/testprogram.java
@@ -5,6 +5,7 @@ public class testprogram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World!");
+		System.out.println("Hello Eclipse");'\
 
 	}
 
